111a3a8fc31fdb45595c20dcff22955a9af757fb
Max Boston <maxboston@berkeley.edu>
2023-03-17 01:16:26 -0700

    wordnet progress

--- proj2b/ngordnet/wordnet/WordnetGraph.java
+++ proj2b/ngordnet/wordnet/WordnetGraph.java
@@ -1,10 +1,26 @@
 package ngordnet.wordnet;
 
-public class WordnetGraph {
+import java.util.HashMap;
+import java.util.List;
 
-    private DirectedGraph graph;
+public class WordnetGraph {
+    //TODO: implement method for file --> graph
+    //TODO: graph traversal --> find hyponyms of synset ID (remove duplicates [add to set], alphabetize [sort set])
+    //TODO: convert from synset ID to word
+    private DirectedGraph hyponymsGraph;
+    private HashMap<Integer, List<String>> idKeys;
+    private HashMap <String, Integer> wordKeys;
 
     public WordnetGraph(String synsetFile, String hyponymFile) {
 
     }
+
+    public List<String> IDConvert(int synsetID) {
+        return null;
+    }
+
+    public Integer WordConvert(String word) {
+        return 0;
+    }
+
 }
