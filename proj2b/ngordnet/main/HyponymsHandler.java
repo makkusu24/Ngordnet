ae168f778faa224be47e46024b3211b5a8720291
Max Boston <maxboston@berkeley.edu>
2023-03-16 23:01:04 -0700

    handler, main updated

--- proj2b/ngordnet/main/HyponymsHandler.java
+++ proj2b/ngordnet/main/HyponymsHandler.java
@@ -3,11 +3,25 @@ package ngordnet.main;
 import ngordnet.browser.NgordnetQuery;
 import ngordnet.browser.NgordnetQueryHandler;
 import ngordnet.wordnet.WordnetGraph;
+import java.util.List;
 
 public class HyponymsHandler extends NgordnetQueryHandler {
 
+    //WordnetGraph graph;
+
+    /**
+    public HyponymsHandler(WordnetGraph wng) {
+        this.graph = wng;
+    }
+     */
+
     @Override
     public String handle(NgordnetQuery q) {
+        List<String> words =q.words();
+        int startYear = q.startYear();
+        int endYear = q.endYear();
+
+
         String placeholder = "Hello!";
         return placeholder;
     }
--- proj2b/ngordnet/main/Main.java
+++ proj2b/ngordnet/main/Main.java
@@ -14,6 +14,7 @@ public class Main {
         String hyponymFile = "./data/wordnet/hyponyms.txt";
 
         NGramMap ngm = new NGramMap(wordFile, countFile);
+        //WordnetGraph wng = new WordNetGraph(synsetFile, hyponymFile);
 
         hns.startUp();
         hns.register("history", new HistoryHandler(ngm));
