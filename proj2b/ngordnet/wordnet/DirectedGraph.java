045afb8c12fb3d4f2ccd2852eb831d44d5a53b08
Max Boston <maxboston@berkeley.edu>
2023-03-16 23:15:34 -0700

    constructors

--- proj2b/ngordnet/wordnet/DirectedGraph.java
+++ proj2b/ngordnet/wordnet/DirectedGraph.java
@@ -3,4 +3,12 @@ package ngordnet.wordnet;
 import java.util.Queue;
 
 public class DirectedGraph  {
+
+    int vertices;
+    int edges;
+
+    public DirectedGraph(int v) {
+        this.vertices = v;
+    }
+
 }
--- proj2b/ngordnet/wordnet/WordnetGraph.java
+++ proj2b/ngordnet/wordnet/WordnetGraph.java
@@ -1,4 +1,10 @@
 package ngordnet.wordnet;
 
 public class WordnetGraph {
+
+    private DirectedGraph graph;
+
+    public WordnetGraph(String synsetFile, String hyponymFile) {
+        
+    }
 }
