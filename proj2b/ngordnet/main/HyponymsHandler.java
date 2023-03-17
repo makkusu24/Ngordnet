59910ce75fe82bb83b827aa29236d7e166beef08
Max Boston <maxboston@berkeley.edu>
2023-03-16 22:17:18 -0700

    started skeleton

---
+++ proj2b/ngordnet/main/HyponymsHandler.java
@@ -0,0 +1,12 @@
+package ngordnet.main;
+
+import ngordnet.browser.NgordnetQuery;
+import ngordnet.browser.NgordnetQueryHandler;
+
+public class HyponymsHandler extends NgordnetQueryHandler {
+
+    @Override
+    public String handle(NgordnetQuery q) {
+        return "Hello!";
+    }
+}
